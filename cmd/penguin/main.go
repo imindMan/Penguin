4b4aaba869b430bc26df0a937f532a92dc828b9c
AnAverageBeing <yatharthrathi.rathi@gmail.com>
2023-03-08 14:23:05 +0530

    ok

--- cmd/penguin/main.go
+++ cmd/penguin/main.go
@@ -7,25 +7,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 )
 
 func main() {
-	// Check if an input file was provided
-	args := os.Args
-	if len(args) < 2 {
-		log.Fatalf("No input file provided\n")
-	}
-
-	// Open the input file
-	inputFile := args[1]
-	file, err := os.Open(inputFile)
-	if err != nil {
-		fmt.Println("Failed to open input file:", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	// TODO: implement functionality
+	fmt.Printf("It will work soon!")
 }
